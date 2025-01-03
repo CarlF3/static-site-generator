7b55046ef616a61aab701a67aacd69631c5c9a77
CarlF3 <156613411+CarlF3@users.noreply.github.com>
2025-01-03 16:52:56 +0000

    Created LeafNode class and unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,9 +6,11 @@ class HTMLNode():
         self.props = props
 
     def to_html(self):
-        raise NotImplemented
+        raise NotImplemented("to_html method not yet implomented")
     
     def props_to_html(self):
+        if self.props is None:
+            return ""
         html_props = ""
         for prop in self.props.items():
             html_props += (f" {prop[0]}=\"{prop[1]}\"")
@@ -16,3 +18,15 @@ class HTMLNode():
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Missing value")
+        if self.tag is None:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
