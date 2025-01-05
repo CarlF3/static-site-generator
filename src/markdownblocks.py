a624b2fa8e80b80d792d10db710cb5f67f92f79a
CarlF3 <156613411+CarlF3@users.noreply.github.com>
2025-01-05 13:59:34 +0000

    Implemented detecting md block type w/ tests

--- src/markdownblocks.py
+++ src/markdownblocks.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    U_LIST = "unordered_list"
+    O_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = []
     lines = markdown.split("\n\n")
@@ -7,3 +17,64 @@ def markdown_to_blocks(markdown):
         line = line.strip()
         blocks.append(line)
     return blocks
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+    # Is heading?
+    if lines[0][0] == "#":
+        split = lines[0].split(" ")
+        # Max # length is 6, more means incorrect markdown.
+        if len(split[0]) > 6:
+            return BlockType.PARAGRAPH
+        has_error = False
+        for char in split[0]:
+            if not char == "#":
+                has_error = True
+        if has_error:
+            #raise ValueError("Heading starts with non # character")
+            return BlockType.PARAGRAPH
+        return BlockType.HEADING
+    # Is code?
+    if lines[0][0] == "`":
+        if lines[0][:3] == "```" and lines[-1][-3:] == "```":
+            has_error = False
+            for line in lines[1:-2]:
+                if "```" in line:
+                    has_error = True
+            if has_error:
+                #raise ValueError("Markdown code block is contains closing ``` before end")
+                return BlockType.PARAGRAPH
+            return BlockType.CODE
+    # Is quote?
+    if lines[0][0] == ">":
+        has_error = False
+        for line in lines:
+            if not line[0:2] == "> ":
+                has_error = True
+        if has_error:
+            #raise ValueError("A line in markdown quote does not start with '>'")
+            return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    # Is unordered list?
+    if lines[0][0] in "*-":
+        has_error = False
+        for line in lines:
+            if not line[0] in "*-" or not line[1] == " ":
+                has_error = True
+        if has_error:
+            #raise ValueError("Unordered list is not properly formated")
+            return BlockType.PARAGRAPH
+        return BlockType.U_LIST
+    # Is ordered list?
+    if lines[0][0] == "1":
+        has_error = False
+        for i, line in enumerate(lines, 1):
+            digits = len(str(i))
+            if not int(line[0:digits]) == i or not line[digits:digits+2] == ". ":
+                has_error = True
+        if has_error:
+            #raise ValueError("Ordered list is not properly formated")
+            return BlockType.PARAGRAPH
+        return BlockType.O_LIST
+    # Is none of the above, so paragraph?
+    return BlockType.PARAGRAPH
--- src/test_markdownblocks.py
+++ src/test_markdownblocks.py
@@ -1,10 +1,9 @@
 import unittest
-from markdownblocks import markdown_to_blocks
+from markdownblocks import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestMarkdownBlocks(unittest.TestCase):
     def test_markdown_blocks(self):
-        md = """
-# This is a heading
+        md = """# This is a heading
 
 This is a paragraph of text. It has some **bold** and *italic* words inside of it.
 
@@ -17,8 +16,7 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
         self.assertEqual(result, expected)
 
     def test_markdown_newlines(self):
-        md = """
-# This is a heading
+        md = """# This is a heading
 
 
 
@@ -37,6 +35,110 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
         result = markdown_to_blocks(md)
         expected = ["# This is a heading", "This is a paragraph of text. It has some **bold** and *italic* words inside of it.", "* This is the first list item in a list block\n* This is a list item\n* This is another list item"]
         self.assertEqual(result, expected)
+
+    def test_b2bt_heading_1(self):
+        md = """# Heading 1"""
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_b2bt_heading_4(self):
+        md = """#### Heading 4"""
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_b2bt_heading_7(self):
+        md = """####### Heading 7 doesn't exist"""
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_b2bt_code(self):
+        md = """```
+        text1 = "Hello"
+        text2 = "World!"
+        print(f"{text1} {text2}")
+        ```"""
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.CODE)
+
+    def test_b2bt_open_code(self):
+        md = """```
+        text1 = "Hello"
+        text2 = "World!"
+        print(f"{text1} {text2}")
+        """
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_b2bt_broken_code(self):
+        md = """```
+        text1 = "Hello"
+        text2 = "World!" ```
+        print(f"{text1} {text2}")
+        ```"""
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_b2bt_quote(self):
+        md = """> The problem with information that you read on the Internet is that it is not always true.
+> Abraham Lincoln, June 2, 1861"""
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.QUOTE)
+
+    def test_b2bt_broken_quote(self):
+        md = """> The problem with information that you read on the Internet is that it is not always true.
+Abraham Lincoln, June 2, 1861"""
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_b2bt_u_list_dash(self):
+        md = """- Item
+- Another item
+- A final item"""
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.U_LIST)
+
+    def test_b2bt_u_list_asterisk(self):
+        md = """* Item
+* Another item
+* A final item"""
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.U_LIST)
+
+    def test_b2bt_broken_u_list(self):
+        md = """*Item
+*Another item
+*Final item"""
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_b2bt_o_list(self):
+        md = """1. First item
+2. Second item
+3. Third item"""
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.O_LIST)
+
+    def test_b2bt_wrong_start_o_list(self):
+        md = """2. Second item
+3. Third item
+4. Fourth item"""
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_b2bt_broken_o_list(self):
+        md = """2.Second item
+3.Third item
+4.Fourth item"""
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_b2bt_paragraph(self):
+        md = """This is some basic text
+that would go in the markdown
+file as a parahraph."""
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
 
 if __name__ == "__main__":
     unittest.main()
